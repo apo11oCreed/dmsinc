e33137f22388e4d0914734f9580dfedb3bd416e6
nelson <nelson@ncdesigns-studio.com>
2015-05-12 21:27:17 -0400

    Added index_files to gitignore and pic bios on company page

---
+++ company.php
@@ -0,0 +1,60 @@
+<?php
+ 	$page_title='Doctors\' Management Service, Inc. | Our Company';
+	include ('inc/header.inc');
+ ?>
+ <?php
+ 	include ('inc/topNav-company.inc');
+?>
+<div id="screenContainer">
+	<div id="content">
+        <div id="companyTest">
+            <p class="pageIntro">Our clients will tell you how we bring them an increase in pay, organization, and peace of mind.</p>
+            <p class="testimonial"><span class="ldquo"></span><a href="testimonials.php#MDriscoll">Tim is the ultimate professional who will not only assist you with his services but assist you in several areas to help make your company a success &hellip;</a><span class="rdquo"></span></p>
+        </div>
+ 		<div id="pageContentStandard">
+            <div id="staffGeneral">
+                <h1>Our Company</h1>
+                <img src="images/DMS041512.jpg" width="500" height="275" alt="DMS Office" />
+                <p>In 1987, Barbara DiBona decided to chase the American Dream. She was an expert in medical billing. She knew what professionals needed and identified great opportunities in the industry. Barbara knew that smaller client accounts never received the attention they deserved. Her strategy led to a unique design model for Doctors' Management's first clients with intense individual attention and it proved to be fruitful. When larger clients came, Barbara had a surprise for them as well. Instead of personalized service, her idea was group specific services such as chart review and auditing. At the time, the idea was revolutionary. Even now, it is uncommon for billing companies to provide such a service. Barbara's ideas and commitment to her clients yielded close, trusting relationships and even friendships that she still enjoys to this day.</p>
+                <p>Dick DiBona was no stranger to hard work and long hours either. He spent his formative years overseas in the army during the 1960s and returned to a career as a firefighter and a tennis pro. Side by side, Dick and Barbara built the company from scratch. They worked perfectly together. Dick would bring in clients and Barbara would handle the billing. The company grew over the years and eventually moved from Stoughton to South Easton for more space. Dick and Barbara passed on the lessons of hard work to their children by putting them to good use in their teenage years. Little did they know, 25 years later, their children would be groomed to run Doctors' Management Service Inc.</p>
+        		<p>Tim and Rich have been close brothers their whole lives. Even today, their houses are just a 5 minute drive away from each other. They have brought the youth of a second generation to the company and it has yielded great dividends. Rich has always enjoyed working internally at DMS. He is obsessed with understanding, updating, and innovating hardware, data integrity, software, human resources, banking, and all accounting factors for the business. Tim's passion has led him to publishing yearly documentation handbooks since 2009, marketing, communications, and adding services in addition to billing such as Documentation Training and Consultations from strategic group management to education on ACOs, EHRs, and PQRS.</p>
+                <p>DMS now stands as a premier management company with a robust reference list of clients who attest to the model of enhancing services and keeping in strong communication to maximize reimbursement and quality for practices. We commit to adding more complimentary services to our management model each year in our effort to not only add to our comprehensive medical management services, but also remain steadily ahead of the industry.</p>
+                <p>We accept all specialties. There are no practices too large or small. We have solo practitioners as well as groups that provide care for greater than 100,000 patients per year. The decision to change your billing company is a hard one. That is why we invite clients we have worked with to give you the candid subjective perspective to help you determine if we are a good fit. Anyone can claim to provide quality service along with substantial reimbursement. Many of them actually cut the essential services that would raise your income above your competitors. They do this to compete with a rock bottom fee. Our past and current clients will let you know exactly what we have done to bring them an increase in pay, organization, and peace of mind. </p>
+        	</div>
+            <div id="staffMembers">
+                <h1>Our Staff</h1>
+                <div class="memberImgLeft">
+                    <img src="images/image3851.jpg" class="profile" alt="Barbara DiBona" width="171" height="256" />
+                    <h3>Barbara DiBona | <span class="caption">President</span></h3>
+                    <p>Barbara provides structure and confidence as the founder of DMS. She created the company to provide close relationships built on trust and honesty. Her presence keeps the message alive and strong.<br />
+                        <a href="http://www.linkedin.com/pub/barbara-dibona/47/435/414">
+                            <img class="widget" src="images/LinkedIn_Logo30px.png" width="30" height="25" alt="LinkedIn" />
+                        </a>
+                    </p>
+                </div>
+                <div class="memberImgLeft"><img src="images/051115tim.jpg" class="profile" alt="Tim DiBona" width="249" height="402" /><h3>Tim DiBona | <span class="caption">Business Director</span></h3>
+                <p>Tim brings leadership to the staff while establishing great communication and trust with our clients. He forges a delicate balance of an enjoyable environment with a hard working mentality.<br /><a href="http://www.linkedin.com/pub/timothy-dibona/20/452/119"><img class="widget" src="images/LinkedIn_Logo30px.png" width="30" height="25" alt="LinkedIn" /></a></p>
+                </div>
+                <div class="memberImgRight"><img src="images/Rich-Bio-Pic.jpg" class="profile" alt="Rich DiBona" width="191" height="264" /><h3>Rich DiBona | <span class="caption">Director of Finance</span></h3>
+                <p>Rich keeps the company’s finances in order. His ability to answer the customized report requests of our clients is unparalleled. We can always depend on Rich to solve any financial anomaly that pops up.<br /><a href="http://www.linkedin.com/pub/rich-dibona/5/920/572"><img class="widget" src="images/LinkedIn_Logo30px.png" width="30" height="25" alt="LinkedIn" /></a></p>
+                </div>
+                <div class="memberImgLeft"><img src="images/051115jennifer.jpg" class="profile" alt="Jennifer Moniz" width="249" height="402" /><h3>Jennifer Moniz | <span class="caption">Operations Manager</span></h3>
+                <p>Jennifer has established herself as the go-to person for both management and employees alike. This is due to her comprehensive understanding of medical billing and credentialing coupled with her unending work drive.<br /><a href="http://www.linkedin.com/pub/jennifer-moniz/49/a15/36"><img class="widget" src="images/LinkedIn_Logo30px.png" width="30" height="25" alt="LinkedIn" /></a></p>
+                </div>
+                <div class="memberImgRight"><img src="images/051115holly.jpg" class="profile" alt="Holly Davis" width="249" height="402" /><h3>Holly Davis | <span class="caption">Medical Coding Supervisor</span></h3>
+                <p>Holly continues to define her role as supervisor in our coding department. She is a trainer of new hires, a publisher of new coding handbooks, and spearheads the PQRS research and implementation for all our clients.</p>
+                </div>
+                <div class="memberImgLeft"><img src="images/051115alyson.jpg" class="profile" alt="Alyson Silva" width="249" height="402" /><h3>Alyson Silva | <span class="caption">Credentialing Specialist</span></h3>
+                <p>Alyson has become an expert in several departments over her long tenure at DMS. In 2014, the credentialing department required growth and Alyson stepped up to the challenge. She is quickly mastering this very challenging position.</p>
+                </div>
+                <div class="memberImgRight"><img src="images/051115andrea.jpg" class="profile" alt="Andrea Lindback" width="249" height="402" /><h3>Andrea Lindback | <span class="caption">Encounter Entry Supervisor</span></h3>
+                <p>Andrea set the DMS record for reaching the rank of Supervisor over the shortest time period. The majority of data entry specialists were taught under her tutelage. She is intelligent and ambitious.</p>
+                </div>
+            </div>
+        </div>
+ <?php
+	include ('inc/sideBar.inc');
+?>
+ <?php
+ include ('inc/footerCareers.inc');
+ ?>
